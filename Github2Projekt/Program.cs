571b68f2c3ea6c905313c769a171826462d23577
Jakub Krchňáček <krchnja.2025@guh.cz>
2025-05-23 15:34:55 +0200

    První commit tří metod

--- Github2Projekt/Program.cs
+++ Github2Projekt/Program.cs
@@ -6,6 +6,17 @@
         {
             Console.WriteLine("Napsal jsem novou metodu");
         }            
+        public static void  Metoda1()
+        {
+            Console.WriteLine("první metoda");
+        }
+        public static void Metoda2()
+        {
+            Console.WriteLine("druhý metoda");
+        }
+        public static void Metoda3()
+        {
+            Console.WriteLine("třetí metoda");
+        }
     }
-    
 }
