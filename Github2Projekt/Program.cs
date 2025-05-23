1c9b97be067f6672ad926802dd10290169006f4e
Jakub Krchňáček <krchnja.2025@guh.cz>
2025-05-23 15:17:28 +0200

    Odebráno 5 zpráv, přidáno Jméno

--- Github2Projekt/Program.cs
+++ Github2Projekt/Program.cs
@@ -4,11 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World1!");
-            Console.WriteLine("Hello, World2!");
-            Console.WriteLine("Hello, World3!");
-            Console.WriteLine("Hello, World4!");
-            Console.WriteLine("Hello, World5!");
+            Console.WriteLine("Hello, Jakub!");
         }
     }
 }
