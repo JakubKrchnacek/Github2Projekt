96b3036f29626b1fbdf58b229ea5ca56df4e2e0d
Jakub Krchňáček <krchnja.2025@guh.cz>
2025-05-23 15:27:38 +0200

    Napsal jsem novou metodu

--- Github2Projekt/Program.cs
+++ Github2Projekt/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Jakub!");
-        }
+            Console.WriteLine("Napsal jsem novou metodu");
+        }            
     }
+    
 }
